c94c01633faa6b5e3dda84900bf82d00feb807e7
npreiser <nickpreiser@gmail.com>
2021-11-30 11:35:34 -0800

    updated math on bla

--- controllers/hardware_driver.js
+++ controllers/hardware_driver.js
@@ -8,6 +8,7 @@ var drv = module.exports = {
         // force an exception so we get a stack trace. 
         var bla = "hello_world"
         bla = bla + value; 
+        bla = 1 + value;
         throw new Error('Throw makes it go boom!')
         return bla;
 
