5813f68e24ac491cc3ee4403289dd62b178e8c04
npreiser <nickpreiser@gmail.com>
2022-02-04 08:21:02 -0800

    Update custom_module1.js

--- controllers/custom_module1.js
+++ controllers/custom_module1.js
@@ -7,12 +7,21 @@ var service = module.exports = {
 
         hardwaredriver.driverfunc1(value);
     },
-    customfunc2: function (value) {
+    customfunc2: function (value) {     
+
+        // we need this reviewed please.. now
+        
+
+
+        // is this ok steve +
+
 
         hardwaredriver.driverfunc2(value);
     },
     customfunc3: function (value) {
 
+        // steve this is a the 
+        // please review this comment. 
         hardwaredriver.driverfunc3(value);
     }
 
